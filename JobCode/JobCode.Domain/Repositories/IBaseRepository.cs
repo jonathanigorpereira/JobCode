80d5e1b97f3354605c1d17e0eb42e91d81662740
jonathanigorpereira <jonathanigorpereira@gmail.com>
2025-02-17 08:48:04 -0300

    Base repository Interface

---
+++ JobCode/JobCode.Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using JobCode.Core.Entities;
+
+namespace JobCode.Core.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<bool> AddAsync(T entity, CancellationToken cancellationToken);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken);
+        Task<bool> DeleteAsync(T entity, CancellationToken cancellationToken);
+        Task<List<T>> GetAllAsync(CancellationToken cancellationToken);
+    }
+}
