3f8e2649dde4b57a85928c335f5038ea7640e094
jonathanigorpereira <jonathanigorpereira@gmail.com>
2025-02-19 22:59:36 -0300

    Created Yml build and create Result Pattern Class

---
+++ JobCode/JobCode.CrossCutting/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace JobCode.Infrastructure.CrossCutting.Results
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; private set; }
+        public bool IsFailure => !IsSuccess;
+        public string Message { get; private set; }
+        public T Results { get; private set; }
+
+        private Result(T results, bool isSuccess = true, string message = "")
+        {
+            IsSuccess = isSuccess;
+            Results = results;
+            Message = message;
+        }
+
+        public static Result<T?> Success(T? results)
+            => new Result<T?>(results, true, string.Empty);
+
+        public static Result<T> Failure(string errorMessage)
+            => new Result<T>(default!, false, errorMessage);
+    }
+}
